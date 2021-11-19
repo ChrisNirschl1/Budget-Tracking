f83ffeb928700b462ccb2adc4649bf5c27addfed
Christian Nirschl <christiannirschl6@gmail.com>
2021-11-19 13:52:47 -0800

    redeploy

--- server.js
+++ server.js
@@ -15,12 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-// mongoose.connect("mongodb://localhost/budget", {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   useCreateIndex: true,
-//   useFindAndModify: false
-// });
+
 
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
